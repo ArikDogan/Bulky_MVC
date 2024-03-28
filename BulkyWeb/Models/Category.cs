4a5fc1fbb7eef8eb7a4674a72c4f0ccd2016d8e2
ArikDogan <dogan.arik1789@gmail.com>
2024-03-28 17:01:31 +0300

    First Day Commits

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }  // Primary key of the table
+        [Required]
+        public string Name { get; set; } // Category name
+        public int DisplayOrder { get; set; }
+
+
+    }
+}
